b67e885656fc0872fc850c108e757fbf7b51e5fa
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-25 12:20:59 -0700

    Update TestCustomSubCommand.php

--- examples/customuitest/src/xenialdan/customuitest/subcommand/TestCustomSubCommand.php
+++ examples/customuitest/src/xenialdan/customuitest/subcommand/TestCustomSubCommand.php
@@ -3,7 +3,7 @@
 namespace xenialdan\customuitest\subcommand;
 
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use xenialdan\customui\API as UIAPI;
 use xenialdan\customuitest\Loader;
 
@@ -44,7 +44,7 @@ class TestCustomSubCommand extends SubCommand
     public function execute(CommandSender $sender, array $args)
     {
         // if you want to reload dynamic fields (for example a player list in a dropdown), you also need a reloadUIs call
-        $this->getPlugin()->reloadUIs();
+        Loader::reloadUIs();
         // it does not hurt to call the resetUIs, but might be useless if an UI never changes :)
         UIAPI::showUIbyID($this->getPlugin(), Loader::$uis['customUI'], $sender);
         return true;
